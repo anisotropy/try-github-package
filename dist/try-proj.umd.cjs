74ffb19c9573ff2a035fde965ef16e7602eadb13
Hodoug Joung <j.hodoug@gmail.com>
2023-01-27 13:46:51 +0900

    remove dist

--- dist/try-proj.umd.cjs
+++
@@ -1,1 +0,0 @@
-(function(e,t){typeof exports=="object"&&typeof module<"u"?t(exports):typeof define=="function"&&define.amd?define(["exports"],t):(e=typeof globalThis<"u"?globalThis:e||self,t(e["try-proj"]={}))})(this,function(e){"use strict";function t(n,u){return n+u}function i(n,u){return n-u}e.subtract=i,e.sum=t,Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})});
